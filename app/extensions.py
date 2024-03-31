70bba0581ab8f077093fce6df68601a0a66e094e
Sravan7799 <77678515+Sravan7799@users.noreply.github.com>
2024-03-31 16:06:18 +0530

    Update extensions.py
    added new code in extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,19 @@
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
-# Setup MongoDB here
-# mongo = PyMongo(uri="mongodb://localhost:27017/database")
+
+class MongoDBConnector:
+    def __init__(self):
+        self.client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.client['github_events']
+        self.push_events = self.db['push_events']
+        self.pull_request_events = self.db['pull_request_events']
+        self.merge_events = self.db['merge_events']
+
+    def insert_push_event(self, data):
+        self.push_events.insert_one(data)
+
+    def insert_pull_request_event(self, data):
+        self.pull_request_events.insert_one(data)
+
+    def insert_merge_event(self, data):
+        self.merge_events.insert_one(data)
