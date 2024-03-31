498a826cbb0f49e5a7d83780b0baa9d8c8850a37
Sravan7799 <77678515+Sravan7799@users.noreply.github.com>
2024-03-31 16:03:17 +0530

    Update routes.py
    first commit

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,51 @@
-from flask import Blueprint, json, request
+from flask import Flask, jsonify, request
+import json
+from datetime import datetime
+from mongodb_connector.mongodb import MongoDBConnector
 
-webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
-@webhook.route('/receiver', methods=["POST"])
-def receiver():
-    return {}, 200
+app = Flask(__name__)
+mongo = MongoDBConnector()
+
+
+@app.route('/webhook', methods=['POST'])
+def receive_webhook():
+    data = request.json
+    action = data['action']
+    timestamp = datetime.now().strftime('%d %B %Y - %I:%M %p UTC')
+
+    if action == 'push':
+        payload = {
+            'author': data['pusher']['name'],
+            'to_branch': data['ref'].split('/')[-1],
+            'timestamp': timestamp
+        }
+        mongo.insert_push_event(payload)
+    elif action == 'pull_request':
+        payload = {
+            'author': data['sender']['login'],
+            'from_branch': data['pull_request']['head']['ref'],
+            'to_branch': data['pull_request']['base']['ref'],
+            'timestamp': timestamp
+        }
+        mongo.insert_pull_request_event(payload)
+    elif action == 'merge':
+        payload = {
+            'author': data['sender']['login'],
+            'from_branch': data['pull_request']['head']['ref'],
+            'to_branch': data['pull_request']['base']['ref'],
+            'timestamp': timestamp
+        }
+        mongo.insert_merge_event(payload)
+
+    return 'Webhook received successfully'
+
+
+@app.route('/get_events', methods=['GET'])
+def get_events():
+    events = mongo.get_all_events()
+    return jsonify(events)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
